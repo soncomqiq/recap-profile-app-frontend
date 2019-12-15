5873ca8a8a07ceede3033bc8574f21b9e163afc5
soncomqiq <sonter@buzzfreeze.com>
2019-12-15 16:44:45 +0700

    finish 3 pages

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import { Row, Col, Input, Button } from 'antd'
 
 export default class ChangePassword extends Component {
   render() {
     return (
-      <div>
-        Change password
-      </div>
+      <Row style={{ height: '100%' }} type='flex' align="middle" justify="center">
+        <Col>
+          <Row>
+            <Col>
+              <Row>
+                Old password:
+                <Input placeholder="Old password" />
+              </Row>
+              <Row>
+                New password:
+                <Input placeholder="New password" />
+              </Row>
+              <Row>
+                Confirm New password:
+                <Input placeholder="Confirm New password" />
+              </Row>
+            </Col>
+          </Row>
+          <Row type="flex" justify="center" style={{ marginTop: '10px' }}>
+            <Button>Change password</Button>
+          </Row>
+        </Col>
+      </Row>
     )
   }
 }
