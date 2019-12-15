9352c236483d26009237b41f135a3f6c6111e546
soncomqiq <sonter@buzzfreeze.com>
2019-12-15 14:27:04 +0700

    step 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import { Route, Switch } from 'react-router-dom'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import ChangePassword from './pages/ChangePassword';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        <Route route="/" component={Login} />
+        <Route route="/signup" component={Signup} />
+        <Route route="/profile" component={Profile} />
+        <Route route="/changepassword" component={ChangePassword} />
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
